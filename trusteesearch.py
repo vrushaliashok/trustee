021fbabcc7d6dcad5a8df2939f1bb4eb6579dec6
Vrushali Ashok <vrushali.edu@gmail.com>
2023-10-29 12:11:00 +0000

    first commit

---
+++ trusteesearch.py
@@ -0,0 +1,34 @@
+from googlesearch import search
+from newspaper import Article
+from newspaper.article import ArticleException 
+import streamlit as st
+import nltk
+
+nltk.download('punkt')
+st.title("Trustee Recruitment")
+
+def article_content(url):
+    article = Article(url)
+    
+    try:
+        article.download()
+        article.parse()
+        article.nlp()
+        st.write(article.summary)
+    except ArticleException as e:
+        print("Error while processing article:", e)
+
+def search_and_extract(query):
+    search_results = search(query)
+    for i, result in enumerate(search_results, start=1):
+        if result in visited_links:
+            continue
+        visited_links.add(result)
+        st.subheader(result)
+        article_content(result)
+
+if __name__ == "__main__":
+    visited_links = set()
+    search_queries = ['charity trustee recruitment uk', 'effective ways to recruit a trustee', 'points to remember while finding charity trustees']
+    for search_query in search_queries:
+        search_and_extract(search_query)
